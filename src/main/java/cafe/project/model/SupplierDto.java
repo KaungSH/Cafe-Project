5d983177b678444d83e284ae23d50dd0670cc6bf
NayzaryLinn <nayzarlinn921@gmail.com>
2026-09-12 20:30:11 +0630

    Add Supplier CRUD

---
+++ src/main/java/cafe/project/model/SupplierDto.java
@@ -0,0 +1,66 @@
+package cafe.project.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class SupplierDto {
+	private String supplier_id;
+	private String name;
+	private String contact_info;
+	private boolean isdeleted;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	private LocalDateTime created_at;
+
+	public SupplierDto() {
+	}
+
+	public SupplierDto(String supplier_id, String name, String contact_info, boolean isdeleted,
+			LocalDateTime created_at) {
+		this.supplier_id = supplier_id;
+		this.name = name;
+		this.contact_info = contact_info;
+		this.isdeleted = isdeleted;
+		this.created_at = created_at;
+	}
+
+	public String getSupplier_id() {
+		return supplier_id;
+	}
+
+	public void setSupplier_id(String supplier_id) {
+		this.supplier_id = supplier_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContact_info() {
+		return contact_info;
+	}
+
+	public void setContact_info(String contact_info) {
+		this.contact_info = contact_info;
+	}
+
+	public boolean isIsdeleted() {
+		return isdeleted;
+	}
+
+	public void setIsdeleted(boolean isdeleted) {
+		this.isdeleted = isdeleted;
+	}
+
+	public LocalDateTime getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(LocalDateTime created_at) {
+		this.created_at = created_at;
+	}
+}
---
+++ src/main/java/cafe/project/service/SupplierService.java
@@ -0,0 +1,79 @@
+package cafe.project.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import cafe.project.model.SupplierDto;
+import cafe.project.repository.SupplierRepository;
+import cafe.project.repository.entities.Supplier;
+
+@Service
+public class SupplierService {
+
+	private final SupplierRepository repo;
+
+	public SupplierService(SupplierRepository repo) {
+		this.repo = repo;
+	}
+
+	public List<SupplierDto> findAll() {
+		List<Supplier> entities = this.repo.findAll();
+		List<SupplierDto> suppliers = entities.stream().map(this::toDto).toList();
+		return suppliers;
+	}
+
+	public SupplierDto findById(String supplier_id) {
+		Supplier entity = this.repo.findById(supplier_id);
+		if (entity == null)
+			return null;
+		return toDto(entity);
+	}
+
+	public int add(SupplierDto dto) {
+		Supplier entity = toEntity(dto);
+		return this.repo.save(entity);
+	}
+
+	public int edit(String supplier_id, SupplierDto dto) {
+		Supplier entity = toEntity(dto);
+		return this.repo.edit(supplier_id, entity);
+	}
+
+	public int delete(String supplier_id) {
+		return this.repo.delete(supplier_id);
+	}
+
+	public List<SupplierDto> findDeleted() {
+		return this.repo.findDeleted().stream().map(this::toDto).toList();
+	}
+
+	public int restore(String supplier_id) {
+		return this.repo.restore(supplier_id);
+	}
+
+	public int realDelete(String supplier_id) {
+		return this.repo.realDelete(supplier_id);
+	}
+
+	private SupplierDto toDto(Supplier entity) {
+		SupplierDto dto = new SupplierDto();
+		dto.setSupplier_id(entity.getSupplier_id());
+		dto.setName(entity.getName());
+		dto.setContact_info(entity.getContact_info());
+		dto.setIsdeleted(entity.isIsdeleted());
+		dto.setCreated_at(entity.getCreated_at());
+		return dto;
+	}
+
+	private Supplier toEntity(SupplierDto dto) {
+		Supplier entity = new Supplier();
+		entity.setSupplier_id(dto.getSupplier_id());
+		entity.setName(dto.getName());
+		entity.setContact_info(dto.getContact_info());
+		entity.setIsdeleted(dto.isIsdeleted());
+		entity.setCreated_at(dto.getCreated_at());
+		return entity;
+
+	}
+}
