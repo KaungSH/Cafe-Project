5c52630557ffbdcafda72ae6f0bf7617c4585d3c
heinminhtet0505-glitch <heinminhtet0505@gmail.com>
2026-09-12 12:54:47 +0630

    category and unit CRUD commit

---
+++ New folder/src - Copy/main/java/cafe/project/repository/mapper/UnitMapper.java
@@ -0,0 +1,33 @@
+package cafe.project.repository.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import cafe.project.repository.entities.Unit;
+
+
+
+public class UnitMapper implements RowMapper<Unit> {
+
+    @Override
+    public Unit mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        Unit unit = new Unit();
+
+        unit.setUnit_id(rs.getString("unit_id"));
+        unit.setEmployee_id(rs.getString("employee_id"));
+        unit.setName(rs.getString("name"));
+        unit.setAbbreviation(rs.getString("abbreviation"));
+        unit.setIs_active(rs.getBoolean("is_active"));
+        unit.setIsdeleted(rs.getBoolean("isedited"));
+        unit.setIsdeleted(rs.getBoolean("isdeleted"));
+
+        unit.setCreated_at(
+                rs.getTimestamp("created_at").toLocalDateTime()
+        );
+
+        return unit;
+    }
+}
