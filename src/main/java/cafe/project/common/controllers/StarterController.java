00712f847e1d881742df59bd4fb8445b2eca3192
KaungSH <sattheinkaung@gmail.com>
2026-09-14 01:28:33 +0630

    Name Change And Structuring Branch YatiWinLatt

---
+++ src/main/java/cafe/project/common/controllers/StarterController.java
@@ -0,0 +1,14 @@
+package cafe.project.common.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StarterController {
+	
+	@GetMapping("/")
+	public String starter() {
+		return "common/starter";
+	}
+
+}
